1d1c057ec5666cd6d30e5bb9156f11032540c021
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-04-18 09:23:25 +0100

    Implemented movie methods

--- Store/admin.js
+++ Store/admin.js
@@ -1,4 +1,6 @@
 var adminsDB = require("../database").admins;
+// var moviesDB = require("../database").movies;
+var Movie = require("./movie");
 
 function Admin(name, email, password) {
   this.name = name;
@@ -6,3 +8,29 @@ function Admin(name, email, password) {
   this.password = password;
   this.id = adminsDB.length > 0 ? adminsDB[adminsDB.length - 1].id + 1 : 1;
 }
+
+Admin.prototype.addMovie = function(title, genre, quantity, format, year) {
+  Movie.createMovie(title, genre, quantity, format, year);
+};
+
+Admin.prototype.getMoviefromDB = function(title) {
+  var movie = Movie.getMovie(title);
+  return movie;
+};
+
+Admin.prototype.readAllMovies = function() {
+  var collection = Movie.getAllMovies();
+
+  if (collection) {
+    return collection;
+  }
+  return "Err";
+};
+
+Admin.prototype.editMovie = function(title, updateObj) {
+  return Movie.editMovie(title, updateObj);
+};
+
+Admin.prototype.deleteMovie = function(title, year) {
+  return Movie.deleteMovie(title, year);
+};
