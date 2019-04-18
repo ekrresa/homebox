0af4ceee1f915da35317be56bea670e569dbf6b9
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-04-18 09:15:32 +0100

    Built methods to edit and delete a movie

--- Store/movie.js
+++ Store/movie.js
@@ -7,12 +7,12 @@ function Movie(title, genre, quantity, format, year) {
   this.format = format;
   this.year = year;
   this.id = moviesDB.length > 0 ? moviesDB[moviesDB.length - 1].id + 1 : 1;
+
+  moviesDB.push(this);
 }
 
 Movie.createMovie = function(title, genre, quantity, format, year) {
-  var movie = new Movie(title, genre, quantity, format, year);
-  moviesDB.push(movie);
-  return movie;
+  return new Movie(title, genre, quantity, format, year);
 };
 
 Movie.getMovie = function(name) {
@@ -24,6 +24,37 @@ Movie.getMovie = function(name) {
   return false;
 };
 
-Movie.editMovie = function(updateObj) {};
+Movie.getAllMovies = function() {
+  if (moviesDB.length > 0) {
+    return moviesDB;
+  }
+  return false;
+};
+
+Movie.editMovie = function(title, updateObj) {
+  var movie = Movie.getMovie(title);
+
+  if (movie) {
+    for (const key in movie) {
+      if (updateObj.hasOwnProperty(key)) {
+        movie[key] = updateObj[key];
+      }
+    }
+    return movie;
+  } else {
+    return "Movie not found";
+  }
+};
+
+Movie.deleteMovie = function(title, year) {
+  var len = moviesDB.length;
+  for (let index = 0; index < len; index++) {
+    if (moviesDB[index].title === title && moviesDB[index].year === year) {
+      moviesDB.splice(index, 1);
+      return "Movie Deleted";
+    }
+  }
+  return "Movie not found";
+};
 
 module.exports = Movie;
