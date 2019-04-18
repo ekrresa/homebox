0c1b7441398c0cd5ee20cb55b0b69da6d87498a4
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-04-18 12:45:56 +0100

    Built GoldUser function

---
+++ User/goldUser.js
@@ -0,0 +1,24 @@
+var User = require("./user");
+
+function GoldUser(name, email) {
+  User.call(this, name, email);
+  this.cart = [];
+  this.level = "gold";
+}
+
+GoldUser.prototype = Object.create(User.prototype);
+GoldUser.prototype.constructor = GoldUser;
+
+GoldUser.prototype.rentMovie = function(title) {
+  if (this.cart.length <= 2) {
+    var movie = this.readMovie(title);
+    this.cart.push(movie);
+    return "Movie added to cart";
+  }
+  return "Sorry. Can't rent more than two movies. Please upgrade to platinum";
+};
+
+// var mike = new GoldUser("mike", "mike@gmail.com", "pass");
+// mike.rentMovie("Fear");
+// mike.rentMovie("Star Trek");
+// console.log(mike.cart);
