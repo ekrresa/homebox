52ee976b7d63ca837cf0bfd66da77a2a202faaf7
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-04-18 10:28:11 +0100

    changed format field to price

--- Store/movie.js
+++ Store/movie.js
@@ -1,18 +1,18 @@
 var moviesDB = require("../database").movies;
 
-function Movie(title, genre, quantity, format, year) {
+function Movie(title, genre, quantity, price, year) {
   this.title = title;
   this.genre = genre;
   this.quantity = quantity;
-  this.format = format;
+  this.price = price.toFixed(2);
   this.year = year;
   this.id = moviesDB.length > 0 ? moviesDB[moviesDB.length - 1].id + 1 : 1;
 
   moviesDB.push(this);
 }
 
-Movie.createMovie = function(title, genre, quantity, format, year) {
-  return new Movie(title, genre, quantity, format, year);
+Movie.createMovie = function(title, genre, quantity, price, year) {
+  return new Movie(title, genre, quantity, price, year);
 };
 
 Movie.getMovie = function(name) {
