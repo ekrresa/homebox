d2bd69b5dfad036d4262c65585ce681cda12b464
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-04-19 14:31:20 +0100

    Add id field to movies array

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 var admins = [];
 var customers = [];
 var movies = [
-  { title: "Fear", genre: "Thriller", quantity: 30, price: 50, year: 1997 },
-  { title: "Star Trek", genre: "Sci-Fi", quantity: 10, price: 90, year: 2005 }
+  { id: 1, title: "Fear", genre: "Thriller", quantity: 30, price: 50, year: 1997 },
+  { id: 2, title: "Star Trek", genre: "Sci-Fi", quantity: 10, price: 90, year: 2005 }
 ];
 var rentals = [];
 
