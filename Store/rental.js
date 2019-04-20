de13799f84279119557e24597df246e0de8509d2
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-04-20 09:50:57 +0100

    Built rental function

---
+++ Store/rental.js
@@ -0,0 +1,20 @@
+var rentalDB = require("../database").rentals;
+
+function Rental(id, cart) {
+  var date = new Date();
+  this.id = rentalDB.length > 0 ? rentalDB[rentalDB.length - 1].id + 1 : 1;
+  this.customer_id = id;
+  this.movies = cart;
+  this.status = "Not Due";
+  this.rentDate = date.toDateString();
+  this.dueDate = addDays(5);
+}
+
+// Add days to current date
+function addDays(days) {
+  var time = new Date().getTime();
+  var newTime = time + 86400000 * days;
+  return new Date(newTime).toDateString();
+}
+
+module.exports = Rental;
